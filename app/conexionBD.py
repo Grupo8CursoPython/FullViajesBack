090b7307c292c23df7e0cbd66826454eae82ed05
Devops295 <devops295@gmail.com>
2024-06-25 18:22:24 -0400

    Desafio Back Grupo 8

--- app/conexionBD.py
+++ app/conexionBD.py
@@ -2,44 +2,16 @@
 #Importando Libreria mysql.connector para conectar Python con MySQL
 import mysql.connector
 
-#def connectionBD():
-#   mydb = mysql.connector.connect(
-#       host ="http://127.0.0.1",
-#       user ="izquierdow",
-#        passwd ="Neopotenza9000*",
-#        database = "ddbbback"
-#        )
-#   if mydb:
-#        print ("Conexion exitosa a BD")
-#        return mydb
-#   else:
-#        print("Error en la conexion a BD")
-        
+       
 def connectionBD():
     mydb = mysql.connector.connect(
-        host ="mysql-grupo8python.alwaysdata.net",
-        user ="366420",
-        passwd ="Curso8Python",
-        database = "grupo8python_ddbb_ti02"
+        host ="xxxxxxxx",
+        user ="xxxxx",
+        passwd ="xxxxxxx",
+        database = "xxxxxxx"
         )
     if mydb:
         print ("Conexion exitosa a BD")
         return mydb
     else:
         print("Error en la conexion a BD")
-
-#Importando Libreria mysql.connector para conectar Python con MySQL
-#import mysql.connector
-
-#def connectionBD():
-#    mydb = mysql.connector.connect(
-#        host ="izquierdow.mysql.pythonanywhere-services.com",
-#        user ="izquierdow",
-#        passwd ="Neopotenza9000*",
-#        database = "izquierdow$default"
-#        )
-#    if mydb:
-#        print ("Conexion exitosa a BD")
-#        return mydb
-#    else:
-#        print("Error en la conexion a BD")
